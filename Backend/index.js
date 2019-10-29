3b601099bdf3eec3cb4a44d7860699638787fb4e
hainsworth99 <31012794+hainsworth99@users.noreply.github.com>
2019-10-29 16:57:36 -0600

    added CRUD API endpoints for assemblies

--- Backend/index.js
+++ Backend/index.js
@@ -58,7 +58,15 @@ app.get('/api/parts/:id', (req, res) => {
 
 // update part (basic CRUD)
 app.put('/api/parts/:id',(req, res) => {
-    let sql = "UPDATE part SET PartID='" + req.body.PartID + "', PartName='" + req.body.PartName + "', PartDescription='" + req.body.PartDescription + "', QuantityPerPops='" + req.body.QuantityPerPops + "', CostPerUnit='" + req.body.CostPerUnit + "', URLForReorder='" + req.body.URLForReorder + "', CurrentInventoryCount='" + req.body.CurrentInventoryCount + "', PartNotes='" + req.body.PartNotes + "' WHERE PartID='" + req.params.id + "'";
+    let sql = "UPDATE part SET PartID='" + req.body.PartID +
+                        "', PartName='" + req.body.PartName +
+                        "', PartDescription='" + req.body.PartDescription +
+                        "', QuantityPerPops='" + req.body.QuantityPerPops +
+                        "', CostPerUnit='" + req.body.CostPerUnit +
+                        "', URLForReorder='" + req.body.URLForReorder +
+                        "', CurrentInventoryCount='" + req.body.CurrentInventoryCount +
+                        "', PartNotes='" + req.body.PartNotes +
+                         "' WHERE PartID='" + req.params.id + "'";
     let query = conn.query(sql, (err, results) => {
         if(err) throw err;
         res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
@@ -88,19 +96,53 @@ app.get('/api/assemblies', (req, res) => {
     });
 });
 
-// get assembly
-app.get('/api/assemblies/:id', (req, res) => {
-    console.log(req.url)
-
-    let sql = "SELECT a.AssemblyName, parts.* FROM assembly a, (SELECT pha.Assembly_AssemblyID, p.PartName, p.PartID, pha.PartCountPerAssembly FROM part p, Part_has_Assembly pha WHERE p.PartID = pha.Part_PartID ORDER BY pha.Assembly_AssemblyID) as parts WHERE a.AssemblyID = parts.Assembly_AssemblyID AND a.AssemblyID = '" + req.params.id + "'";
-
-    let query = conn.query(sql, (err, results) => {
+// create new assembly (basic CRUD)
+app.post('/api/assemblies', (req, res) => {
+    let sql = "INSERT INTO assembly SET ?";
+    let data = {
+        AssemblyID: req.body.AssemblyID,
+        AssemblyName: req.body.AssemblyName,
+        CurrentInventoryCount: req.body.CurrentInventoryCount,
+        AssemblyNotes: req.body.AssemblyNotes,
+        ParentAssembly: req.body.ParentAssembly
+    }
+    let query = conn.query(sql, data, (err, results) => {
         if(err) throw err;
-        console.log(results)
         res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
     });
 });
 
+// read assembly (basic CRUD)
+app.get('/api/assemblies/:id', (req, res) => {
+    let sql = "SELECT * FROM assembly WHERE AssemblyID = '" + req.params.id + "'";
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+});
+
+// update assembly (basic CRUD)
+app.put('/api/assemblies/:id', (req, res) => {
+    let sql = "UPDATE assembly SET AssemblyID = '" + req.body.AssemblyID +
+        "', AssemblyName = '" + req.body.AssemblyName +
+        "', CurrentInventoryCount = '"+ req.body.CurrentInventoryCount +
+        "', AssemblyNotes = '" + req.body.AssemblyNotes +
+        "', ParentAssembly = '" + req.body.ParentAssembly +
+        "' WHERE AssemblyID = '" + req.body.AssemblyID + "'";
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+});
+
+// delete assembly (basic CRUD)
+app.delete('/api/assemblies/:id', (req, res) => {
+    let sql = "DELETE FROM assembly WHERE AssemblyID = '" + req.params.id + "'";
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000...');
